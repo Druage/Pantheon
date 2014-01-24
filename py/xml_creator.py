d679aac008ee019a333ed3d847eb6db5c37dd05f
Druage <Druage@gmx.com>
2014-01-23 22:19:30 -0500

    Will now create .xml data in alphabetical order.

--- py/xml_creator.py
+++ py/xml_creator.py
@@ -47,57 +47,61 @@ def xmlwriter(data): #xmlwriter() is loaded automatically after scan is run
         tree = open("games.xml" ,"w")
         tree.write("""<?xml version="1.0"?>\n""")
         root = Element("Library")
-        for key in data: #removes extensions for filenames for key values
-            game = ET.SubElement(root, "game")
-            system = ET.SubElement(game, "system")
-            title = ET.SubElement(game, "title")
-            path = ET.SubElement(game, "path")
-            core = ET.SubElement(game, "core")
+        #print(data.items())
+        try:
+            for key, value in sorted(data.items()): #removes extensions for filenames for key values
+                print(key)
+                print(value)
+                game = ET.SubElement(root, "game")
+                system = ET.SubElement(game, "system")
+                title = ET.SubElement(game, "title")
+                path = ET.SubElement(game, "path")
+                core = ET.SubElement(game, "core")
 
-            extension = key[-4::].lower() #Play it safe for those odd ALL CAPS e.g. (.NES) extensions
-            core_ext = ".dll"
-            while True:          
-                if PLATFORM == "win32":
-                    
-                    if ".sfc" in  extension or ".smc" in extension: #Adds system data to xml
-                        system.text = "Super Nintendo"
-                        core.text = "bsnes_balanced_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-4::]) #Must fix for .gb files
-                    elif ".n64" in extension or ".z64" in extension:
-                        system.text = "Nintendo 64"
-                        core.text = "mupen64plus_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-4::])
-                    elif ".gba" in extension:
-                        system.text = "Game Boy Advance"
-                        core.text = "vbam_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-4::])
-                    elif ".cue" in extension:
-                        system.text = "Sony PlayStation"
-                        core.text = "mednafen_psx_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-4::])
-                    elif ".nes" in extension:
-                        system.text = "Nintendo"
-                        core.text = "nestopia_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-4::])
-                    elif ".gb" in extension:
-                        system.text = "Game Boy"
-                        core.text = "gambatte_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-3::])      
-                    elif ".gbc" in extension:
-                        system.text = "Game Boy"
-                        core.text = "gambatte_libretro{:}".format(core_ext)
-                        clean_key = key.strip(key[-4::])
+                extension = key[-4::].lower() #Play it safe for those odd ALL CAPS e.g. (.NES) extensions
+                core_ext = ".dll"
+                while True:          
+                    if PLATFORM == "win32":
+                        if ".sfc" in  extension or ".smc" in extension:
+                            system.text = "Super Nintendo"
+                            core.text = "bsnes_balanced_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-4::]) #Must fix for .gb files
+                        elif ".n64" in extension or ".z64" in extension:
+                            system.text = "Nintendo 64"
+                            core.text = "mupen64plus_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-4::])
+                        elif ".gba" in extension:
+                            system.text = "Game Boy Advance"
+                            core.text = "vbam_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-4::])
+                        elif ".cue" in extension:
+                            system.text = "Sony PlayStation"
+                            core.text = "mednafen_psx_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-4::])
+                        elif ".nes" in extension:
+                            system.text = "Nintendo"
+                            core.text = "nestopia_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-4::])
+                        elif ".gb" in extension:
+                            system.text = "Game Boy"
+                            core.text = "gambatte_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-3::])      
+                        elif ".gbc" in extension:
+                            system.text = "Game Boy"
+                            core.text = "gambatte_libretro{:}".format(core_ext)
+                            clean_key = key.strip(key[-4::])
+                        else:
+                            system.text = "Unknown"
+                            clean_key = key.strip(key[-4::])
+                        break
                     else:
-                        system.text = "Unknown"
-                        clean_key = key.strip(key[-4::])
-                    break
-                else:
-                    core_ext = ".so"
-                    continue
+                        core_ext = ".so"
+                        continue
+                title.text = clean_key
+                path.text = '"{:}"'.format(value)
             
-            title.text = clean_key
-            path.text = '"{:}"'.format(data[key])
-            
+        except:
+            return "Error, .xml not generated"
         indent(root) #root doesn't have to be returned
         tree.write(ET.tostring(root).decode("utf-8"))
         tree.close()
