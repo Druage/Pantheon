56b3b3ed25e048fa88c492ab65a321bf5150f71a
unknown <druage@gmx.com>
2014-05-06 17:15:10 -0400

    added shader.h, library.h, shader.h, and cfg.h

--- main.cpp
+++ main.cpp
@@ -3,17 +3,23 @@
 #include <QQuickWindow>
 #include <QQmlContext>
 #include <QtQml/QQmlEngine>
-#include <qqml.h>
 
-#include <capturer.h>
-#include <scanner.h>
+#include <qqml.h>
+#include "capturer.h"
+#include "library.h"
+#include "launch.h"
+#include "cfg.h"
+#include "shader.h"
 
 
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
 
-    qmlRegisterType<Scanner>("Scanner", 1, 0, "Scanner");
+    qmlRegisterType<Library>("Library", 1, 0, "Library");
+    qmlRegisterType<Launch>("Launch", 1, 0, "Launch");
+    qmlRegisterType<Config>("Config", 1, 0, "Config");
+    qmlRegisterType<Shader>("Shader", 1, 0, "Shader");
 
     QQmlApplicationEngine engine(QUrl("src/qml/main.qml"));
     QObject *topLevel = engine.rootObjects().value(0);
